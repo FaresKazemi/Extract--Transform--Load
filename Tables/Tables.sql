2017c83e25810061e5a77b8c18f6a0e2d40e3691
Fares Kazemi <Fareskazemi@live.com.au>
2022-06-20 12:38:22 +0800

    Loading DATA to SQL Base

---
+++ Tables/Tables.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE "Gun_Violence" (
+    "Incident_ID" INT   NOT NULL,
+    "Incident_Date" date   NOT NULL,
+    "State" VARCHAR   NOT NULL,
+    "City_County" VARCHAR   NOT NULL,
+    "Killed" INT   NOT NULL,
+    "Injured" INT   NOT NULL
+);
+
+CREATE TABLE "Mass_shootings" (
+    "Incident_ID" INT   NOT NULL,
+    "Incident_Date" Date   NOT NULL,
+    "State" VARCHAR   NOT NULL,
+    "City_County" VARCHAR   NOT NULL,
+    "Killed" INT   NOT NULL,
+    "Injured" INT   NOT NULL
+);
